c36efef99f2d026c76e3d3c9c5ed04f1e43d694f
Tzvetlin Velev <tvelev@ameripharma.us>
2024-12-03 10:19:13 -0800

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 const app = express();
-const PORT = 3000;
+const PORT = 5173;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 // Mock database to store users and verification codes
 let users = [];
